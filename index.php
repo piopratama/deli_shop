b599f6a7cfe24a5551b441697a68ca577e56922a
pio pratama <piopratama2@gmail.com>
2018-11-02 15:53:51 +0800

    change to offline reference for css/js bootstrap and jquery

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title></title>
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="./assets/bootstrap3.3.7/css/bootstrap.min.css"">
 		<link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="css/loginStyle.css">
 	</head>
@@ -49,12 +49,12 @@
 					
 						<div class="form-group">
 							<label for="">Username</label>
-							<input type="text" name="username" class="form-control" id="" placeholder="Username" required="required">
+							<input type="text" name="username" class="form-control" id="username" placeholder="Username" required="required">
 						</div>
 						
 						<div class="form-group">
 							<label for="">Password</label>
-							<input type="password" name="password" class="form-control" id="" placeholder="Password" required="required">
+							<input type="password" name="password" class="form-control" id="password" placeholder="Password" required="required">
 						</div>
 						<!-- <div class="form-group">
 							<label>Level</label>
@@ -79,9 +79,9 @@
 			$session_value=(isset($_SESSION['message']))?$_SESSION['message']:'';
 			unset($_SESSION['message']);
 		?>
-		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<script src="./assets/jquery.js"></script>
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="./assets/bootstrap3.3.7/js/bootstrap.min.js"></script>
 		<script>
 			$(document).ready(function() {
 				var message='<?php echo $session_value;?>';
