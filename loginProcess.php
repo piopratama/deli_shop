b2df6e4a9298fe52cfba507daabc8d486a61c380
piopratama <piopratamai@yahoo.com>
2019-01-20 11:49:05 +0800

    PIO - update direct pay

--- loginProcess.php
+++ loginProcess.php
@@ -17,13 +17,14 @@ if(isset($_POST["username"]) && isset($_POST["password"]))
 	  
 	if($b=='1'){
 		
-		$sql = "SELECT id,username FROM tb_employee WHERE username = '$usernamed' AND `password`=MD5('".$password."');";
+		$sql = "SELECT id,username,nama FROM tb_employee WHERE username = '$usernamed' AND `password`=MD5('".$password."');";
 		$result = $conn->query($sql);
 		if($result->num_rows > 0){
 			while($row = $result->fetch_assoc()) {
 				$_SESSION["username"]=$usernamed;
 				$_SESSION["id_kasir"]=$row["id"];
 				$_SESSION["level_user"]=1;
+				$_SESSION["nama"]=$row["nama"];
 			}
 			$sql1 = mysqli_query($conn, "update tb_employee set online_status='1' where username='$usernamed'");
 			header("location:administrator.php");
@@ -34,13 +35,14 @@ if(isset($_POST["username"]) && isset($_POST["password"]))
 	}
 	elseif ($b=='0'){
 		
-		$sql = "SELECT id,username FROM tb_employee WHERE username = '$usernamed' AND `password`=MD5('".$password."');";
+		$sql = "SELECT id,username, nama FROM tb_employee WHERE username = '$usernamed' AND `password`=MD5('".$password."');";
 		$result = $conn->query($sql);
 		if($result->num_rows > 0){
 			while($row = $result->fetch_assoc()){
 				$_SESSION["username"]=$usernamed;
 				$_SESSION["id_kasir"]=$row["id"];
 				$_SESSION["level_user"]=0;
+				$_SESSION["nama"]=$row["nama"];
 			}
 			$sql1 = mysqli_query($conn, "update tb_employee set online_status='1' where username='$usernamed'");
 			header("location:mainMenu.php");
