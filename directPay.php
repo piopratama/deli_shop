05a22833994b7eb6348d1edaff114ac059b58de9
piopratama <piopratamai@yahoo.com>
2019-01-23 08:29:34 +0800

    PIO -

--- directPay.php
+++ directPay.php
@@ -306,11 +306,12 @@ $result = $conn->query($sql);
 				});*/
 
 				function formatDate (input) {
+					var currentdate = new Date();
 					var datePart = input.match(/\d+/g),
 					year = datePart[0], // get only two digits
 					month = datePart[1], day = datePart[2];
 
-					return day+'/'+month+'/'+year;
+					return day+'/'+month+'/'+year+' '+currentdate.getHours()+':'+currentdate.getMinutes()+':'+currentdate.getSeconds();
 				}
 
 				$("#printItem").click(function(event) {
